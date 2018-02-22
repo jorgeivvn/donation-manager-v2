e1a29a2e69982bf29588dc66390f2907d7d243e9
Lillian Chernin <l.smith1654@gmail.com>
2018-02-22 09:55:12 -0600

    fix cookie security address

--- donationManager/settings.py
+++ donationManager/settings.py
@@ -15,7 +15,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-CSRF_COOKIE_DOMAIN = 'localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
